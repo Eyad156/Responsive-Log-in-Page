bdc12777dd31077b1e9f9b4093d14296f5dafb7c
Eyad156 <eyad1509@gmail.com>
2024-03-25 19:57:52 +0200

    Eyad Emad

--- firstpage/main.js
+++ firstpage/main.js
@@ -1,14 +1,14 @@
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault(); 
 
     const email = document.getElementById('e-mail').value;
     const password = document.getElementById('password').value;
 
     if (email === 'Admin@gmail.com' && password === 'admin123') {
-        // Redirect to another page
+        
         window.location.href = '/firstpage/login.html';
-    } else {
-        // Display an error message or take other actions as needed
+    } 
+    else {
         alert('Invalid email or password. Please try again.');
     }
 }
