e95fbb9d6c8489fa6a0ef7d0430b47c2e0d003de
Eyad156 <eyad1509@gmail.com>
2024-03-25 19:50:20 +0200

    Eyad Emad @Driplay

--- firstpage/main.js
+++ firstpage/main.js
@@ -1,3 +1,14 @@
-var email = document.getElementById('e-mail')
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent default form submission
 
+    const email = document.getElementById('e-mail').value;
+    const password = document.getElementById('password').value;
 
+    if (email === 'Admin@gmail.com' && password === 'admin123') {
+        // Redirect to another page
+        window.location.href = '/firstpage/login.html';
+    } else {
+        // Display an error message or take other actions as needed
+        alert('Invalid email or password. Please try again.');
+    }
+}
